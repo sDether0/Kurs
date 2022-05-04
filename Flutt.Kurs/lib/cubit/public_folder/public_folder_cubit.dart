c8ed533d0afcd5aab0adab5f510c14c118020001
Alexandr Krasnov <67280977+propak12@users.noreply.github.com>
2022-05-04 19:07:56 +0300

    public scene and folders navig

--- Flutt.Kurs/lib/cubit/public_folder/public_folder_cubit.dart
+++ Flutt.Kurs/lib/cubit/public_folder/public_folder_cubit.dart
@@ -4,4 +4,19 @@ import 'package:kurs/cubit/public_folder/cubit.dart';
 class PublicFolderCubit extends Cubit<PublicFolderState>{
   PublicFolderCubit() : super(PublicFolderEmptyState());
 
+
+  Future<void> load()async{
+    emit(PublicFolderLoadingState());
+
+
+
+
+
+
+    Future.delayed(const Duration(milliseconds: 100),
+            () => emit(PublicFolderLoadedState()));
+  }
+  Future<void> refresh() async {
+    emit(PublicFolderEmptyState());
+  }
 }
