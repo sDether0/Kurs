ec31280ee0b375cf917a0d9d0141a4db4146ff3b
Alexandr Krasnov <67280977+propak12@users.noreply.github.com>
2022-05-09 02:12:58 +0300

    FAB but need to create file for them

--- Flutt.Kurs/lib/ui/widgets/main_folder_form.dart
+++ Flutt.Kurs/lib/ui/widgets/main_folder_form.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:kurs/cubit/main_folder/cubit.dart';
 import 'package:kurs/cubit/navigation/cubit.dart';
 import 'package:kurs/resources/app_colors.dart';
@@ -25,9 +26,42 @@ class MainFolderForm extends StatelessWidget {
     var _cubit = context.read<MainFolderCubit>();
     var size = MediaQuery.of(context).size;
     return Scaffold(
+      //ОБЯЗАТЕЛЬНО СДЕЛАТЬ ОТДЕЛЬНО
+      floatingActionButton: SpeedDial(animatedIcon: AnimatedIcons.menu_close,
+        overlayOpacity: 0,
+        animatedIconTheme: const IconThemeData(size: 22),
+        backgroundColor: AppColors.primaryColor,
+        visible: true,
+        curve: Curves.bounceIn,
+        children: [
+          // FAB 1
+          SpeedDialChild(
+              child: const Icon(Icons.upload_file),
+              backgroundColor: AppColors.primaryColor,
+              onTap: () { _cubit.uploadFile(); },
+              //label: 'Button 1',
+              labelStyle: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                  fontSize: 16.0),
+              labelBackgroundColor: const Color(0xFF801E48)),
+          // FAB 2
+          SpeedDialChild(
+              child: const Icon(Icons.create_new_folder),
+              backgroundColor: AppColors.primaryColor,
+              onTap: () {},
+              //label: 'Button 2',
+              labelStyle: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                  fontSize: 16.0),
+              labelBackgroundColor: const Color(0xFF801E48))
+        ],),
+      //ВСЁ ЧТО ВЫШЕ СДЕЛАТЬ ОТДЕЛЬНО
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(60),
         child: AppBar(
+          backgroundColor: AppColors.primaryColor,
           centerTitle: true,
           title: Padding(
             padding: const EdgeInsets.only(top: 5),
