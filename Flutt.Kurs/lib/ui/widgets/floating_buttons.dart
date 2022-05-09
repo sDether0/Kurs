969d99ad315863bd21b01a0ae5ddd4102564c37d
Alexandr Krasnov <67280977+propak12@users.noreply.github.com>
2022-05-10 00:19:24 +0300

    432423

--- Flutt.Kurs/lib/ui/widgets/floating_buttons.dart
+++ Flutt.Kurs/lib/ui/widgets/floating_buttons.dart
@@ -2,43 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:kurs/resources/app_colors.dart';
 
-class FloatingMenu extends StatelessWidget
-{
-  const FloatingMenu({required this.func,required this.func1}): super();
+class FloatingMenu extends StatelessWidget {
+  const FloatingMenu({required this.func, required this.func1}) : super();
   final Function func;
   final Function func1;
 
   @override
   Widget build(BuildContext context) {
-    return SpeedDial(animatedIcon: AnimatedIcons.menu_close,
-          overlayOpacity: 0,
-          animatedIconTheme: const IconThemeData(size: 22),
+    return SpeedDial(
+      animatedIcon: AnimatedIcons.menu_close,
+      overlayOpacity: 0,
+      animatedIconTheme: const IconThemeData(size: 22),
+      backgroundColor: AppColors.primaryColor,
+      visible: true,
+      curve: Curves.bounceIn,
+      children: [
+        // FAB 1
+        SpeedDialChild(
+          child: const Icon(Icons.upload_file),
           backgroundColor: AppColors.primaryColor,
-          visible: true,
-          curve: Curves.bounceIn,
-          children: [
-            // FAB 1
-            SpeedDialChild(
-                child: const Icon(Icons.upload_file),
-                backgroundColor: AppColors.primaryColor,
-                onTap: () {func();},
-                //label: 'Button 1',
-                labelStyle: const TextStyle(
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
-                    fontSize: 16.0),
-                labelBackgroundColor: const Color(0xFF801E48)),
-            // FAB 2
-            SpeedDialChild(
-                child: const Icon(Icons.create_new_folder),
-                backgroundColor: AppColors.primaryColor,
-                onTap: () {func1();},
-                //label: 'Button 2',
-                labelStyle: const TextStyle(
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
-                    fontSize: 16.0),
-                labelBackgroundColor: const Color(0xFF801E48))
-          ],);
+          onTap: () {
+            func();
+          },
+        ),
+        // FAB 2
+        SpeedDialChild(
+          child: const Icon(Icons.create_new_folder),
+          backgroundColor: AppColors.primaryColor,
+          onTap: () {
+            func1();
+          },
+        )
+      ],
+    );
   }
 }
