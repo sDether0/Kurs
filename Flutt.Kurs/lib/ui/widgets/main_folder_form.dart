04fae492178ee07fa1eb902afe721d80045a6bee
Alexandr Krasnov <67280977+propak12@users.noreply.github.com>
2022-05-10 02:52:18 +0300

    josko navalil 700 strok koda

--- Flutt.Kurs/lib/ui/widgets/main_folder_form.dart
+++ Flutt.Kurs/lib/ui/widgets/main_folder_form.dart
@@ -117,8 +117,7 @@ class MainFolderForm extends StatelessWidget {
         child: AppBar(
           backgroundColor: AppColors.primaryColor,
           centerTitle: true,
-          leadingWidth: size.width*0.35,
-
+          leadingWidth: size.width * 0.35,
           leading: const Padding(
             padding: EdgeInsets.only(left: 15),
             child: Align(
@@ -136,9 +135,7 @@ class MainFolderForm extends StatelessWidget {
                   textAlign: TextAlign.center,
                   style: const TextStyle(fontSize: 15, fontFamily: 'Arvo'),
                 ),
-          actions: const [
-            LogoutButton()
-          ],
+          actions: const [LogoutButton()],
         ),
       ),
       backgroundColor: AppColors.primaryBackgroundColor,
@@ -196,7 +193,208 @@ class MainFolderForm extends StatelessWidget {
                                         ],
                                       ),
                                     ),
-                                  ));
+                                  ),
+                                  //НАДО ШОТО ДЕЛАТЬ С ЭТИМ ЛОНГ ПРЕССОМ
+                                  onLongPressStart: (details) {
+                                    showMenu(
+                                        context: context,
+                                        position: RelativeRect.fromLTRB(
+                                            details.globalPosition.dx,
+                                            details.globalPosition.dy,
+                                            details.globalPosition.dx,
+                                            details.globalPosition.dy),
+                                        items: [
+                                          PopupMenuItem(
+                                            child: RenameButton(func: () {
+                                              // _cubit.renameFile(
+                                              //     _cubit.mFolder.files[index]);
+                                              showDialog(
+                                                  context: context,
+                                                  builder: (context) {
+                                                    return Dialog(
+                                                      backgroundColor: AppColors
+                                                          .primaryBackgroundColor,
+                                                      shape: RoundedRectangleBorder(
+                                                          side: const BorderSide(
+                                                              color: AppColors
+                                                                  .borderColor,
+                                                              width: 2),
+                                                          borderRadius:
+                                                              BorderRadius
+                                                                  .circular(5)),
+                                                      child: SizedBox(
+                                                        height:
+                                                            size.height * 0.195,
+                                                        width: size.width * 0.2,
+                                                        child: Column(
+                                                          children: [
+                                                            const Padding(
+                                                              padding: EdgeInsets
+                                                                  .only(
+                                                                      top: 15),
+                                                              child: Text(
+                                                                "Rename file/folder",
+                                                                style: TextStyle(
+                                                                    fontSize:
+                                                                        15,
+                                                                    fontFamily:
+                                                                        'Arvo',
+                                                                    color: AppColors
+                                                                        .primaryTextColor),
+                                                              ),
+                                                            ),
+                                                            Padding(
+                                                              padding:
+                                                                  const EdgeInsets
+                                                                          .only(
+                                                                      left: 10,
+                                                                      right: 10,
+                                                                      top: 10),
+                                                              child: Row(
+                                                                mainAxisAlignment:
+                                                                    MainAxisAlignment
+                                                                        .spaceAround,
+                                                                children: [
+                                                                  Expanded(
+                                                                    flex: 3,
+                                                                    child:
+                                                                        TextField(
+                                                                      decoration: InputDecoration(
+                                                                          filled: true,
+                                                                          fillColor: Colors.blue.shade100,
+                                                                          border: OutlineInputBorder(
+                                                                            borderRadius:
+                                                                                BorderRadius.circular(15),
+                                                                            borderSide:
+                                                                                BorderSide.none,
+                                                                          )),
+                                                                      cursorColor:
+                                                                          AppColors
+                                                                              .primaryTextColor,
+                                                                      style: const TextStyle(
+                                                                          fontFamily:
+                                                                              'Arvo'),
+                                                                      controller: Controllers.fileRenameController = TextEditingController(
+                                                                          text: _cubit
+                                                                              .mFolder
+                                                                              .files[index]
+                                                                              .shortName),
+                                                                    ),
+                                                                  ),
+                                                                  Flexible(
+                                                                    child:
+                                                                        SizedBox(
+                                                                      width: 60,
+                                                                      child:
+                                                                          TextField(
+                                                                        decoration: InputDecoration(
+                                                                            filled: true,
+                                                                            fillColor: Colors.blue.shade100,
+                                                                            border: OutlineInputBorder(
+                                                                              borderRadius: BorderRadius.circular(15),
+                                                                              borderSide: BorderSide.none,
+                                                                            )),
+                                                                        cursorColor:
+                                                                            AppColors.primaryTextColor,
+                                                                        style: const TextStyle(
+                                                                            fontFamily:
+                                                                                'Arvo'),
+                                                                        controller:
+                                                                            Controllers.fileExtensionController =
+                                                                                TextEditingController(text: _cubit.mFolder.files[index].ext),
+                                                                      ),
+                                                                    ),
+                                                                  )
+                                                                ],
+                                                              ),
+                                                            ),
+                                                            Padding(
+                                                              padding:
+                                                                  const EdgeInsets
+                                                                          .only(
+                                                                      right:
+                                                                          10),
+                                                              child: Row(
+                                                                //crossAxisAlignment : CrossAxisAlignment.center,
+                                                                mainAxisAlignment:
+                                                                    MainAxisAlignment
+                                                                        .end,
+                                                                children: [
+                                                                  TextButton(
+                                                                    onPressed:
+                                                                        () {
+                                                                      _cubit.renameFile(_cubit
+                                                                          .mFolder
+                                                                          .files[index]);
+                                                                      Controllers
+                                                                              .fileRenameController =
+                                                                          TextEditingController(
+                                                                              text: "");
+                                                                      Navigator.pop(
+                                                                          context);
+                                                                    },
+                                                                    child:
+                                                                        const Text(
+                                                                      "OK",
+                                                                      style:
+                                                                          AppTextStyles
+                                                                              .h3,
+                                                                    ),
+                                                                  ),
+                                                                  TextButton(
+                                                                      onPressed:
+                                                                          () {
+                                                                        Controllers.fileRenameController =
+                                                                            TextEditingController(text: "");
+                                                                        Navigator.pop(
+                                                                            context);
+                                                                      },
+                                                                      child:
+                                                                          const Text(
+                                                                        "Cancel",
+                                                                        style: AppTextStyles
+                                                                            .h3,
+                                                                      ),
+                                                                      style:
+                                                                          const ButtonStyle())
+                                                                ],
+                                                              ),
+                                                            ),
+                                                          ],
+                                                        ),
+                                                      ),
+                                                    );
+                                                  });
+
+                                              Navigator.pop(context);
+                                            }),
+                                          ),
+                                          PopupMenuItem(
+                                            child: DeleteButton(
+                                              func: () {},
+                                              local: false,
+                                            ),
+                                          ),
+                                          (_cubit.mFolder.files[index]
+                                                  .downloaded
+                                              ? PopupMenuItem(
+                                                  child: DeleteButton(
+                                                    func: () {
+                                                      _cubit.deleteFile(_cubit
+                                                          .mFolder
+                                                          .files[index]);
+                                                      Navigator.pop(context);
+                                                    },
+                                                    local: true,
+                                                  ),
+                                                )
+                                              : const PopupMenuItem(
+                                                  child: SizedBox.shrink(),
+                                                  height: 0,
+                                                ))
+                                        ]);
+                                    //ЭТО ЖЕСТЬ 700 СТРОК ИЗЗА ЭТОГО НАДО ШОТО ДЕЛАТЬ
+                                  });
                             }) +
                             List.generate(_cubit.mFolder.files.length, (index) {
                               return GestureDetector(
