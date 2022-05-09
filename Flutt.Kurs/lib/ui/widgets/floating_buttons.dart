602eb148ab0826c6847373051622942d2a414f02
Alexandr Krasnov <67280977+propak12@users.noreply.github.com>
2022-05-10 00:16:29 +0300

    another file for FAB - done

--- Flutt.Kurs/lib/ui/widgets/floating_buttons.dart
+++ Flutt.Kurs/lib/ui/widgets/floating_buttons.dart
@@ -1,48 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
-class FloatingMenu extends StatefulWidget
-{
-  const FloatingMenu():super();
+import 'package:kurs/resources/app_colors.dart';
 
-  Widget _getFAB() {
-    return SpeedDial(
-      animatedIcon: AnimatedIcons.menu_close,
-      animatedIconTheme: const IconThemeData(size: 22),
-      backgroundColor: const Color(0xFF801E48),
-      visible: true,
-      curve: Curves.bounceIn,
-      children: [
-        // FAB 1
-        SpeedDialChild(
-            child: const Icon(Icons.assignment_turned_in),
-            backgroundColor: const Color(0xFF801E48),
-            onTap: () { /* do anything */ },
-            label: 'Button 1',
-            labelStyle: const TextStyle(
-                fontWeight: FontWeight.w500,
-                color: Colors.white,
-                fontSize: 16.0),
-            labelBackgroundColor: const Color(0xFF801E48)),
-        // FAB 2
-        SpeedDialChild(
-            child: const Icon(Icons.assignment_turned_in),
-            backgroundColor: const Color(0xFF801E48),
-            onTap: () {},
-            label: 'Button 2',
-            labelStyle: const TextStyle(
-                fontWeight: FontWeight.w500,
-                color: Colors.white,
-                fontSize: 16.0),
-            labelBackgroundColor: const Color(0xFF801E48))
-      ],
-    );
-  }
+class FloatingMenu extends StatelessWidget
+{
+  const FloatingMenu({required this.func,required this.func1}): super();
+  final Function func;
+  final Function func1;
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  Widget build(BuildContext context) {
+    return SpeedDial(animatedIcon: AnimatedIcons.menu_close,
+          overlayOpacity: 0,
+          animatedIconTheme: const IconThemeData(size: 22),
+          backgroundColor: AppColors.primaryColor,
+          visible: true,
+          curve: Curves.bounceIn,
+          children: [
+            // FAB 1
+            SpeedDialChild(
+                child: const Icon(Icons.upload_file),
+                backgroundColor: AppColors.primaryColor,
+                onTap: () {func();},
+                //label: 'Button 1',
+                labelStyle: const TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                    fontSize: 16.0),
+                labelBackgroundColor: const Color(0xFF801E48)),
+            // FAB 2
+            SpeedDialChild(
+                child: const Icon(Icons.create_new_folder),
+                backgroundColor: AppColors.primaryColor,
+                onTap: () {func1();},
+                //label: 'Button 2',
+                labelStyle: const TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                    fontSize: 16.0),
+                labelBackgroundColor: const Color(0xFF801E48))
+          ],);
   }
 }
-
-
