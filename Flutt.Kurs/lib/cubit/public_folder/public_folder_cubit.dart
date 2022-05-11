820b61d96f4c816358da1804d57ce9ce610ffc70
sDether <51996975+sDether0@users.noreply.github.com>
2022-05-11 13:29:10 +0300

    public folder

--- Flutt.Kurs/lib/cubit/public_folder/public_folder_cubit.dart
+++ Flutt.Kurs/lib/cubit/public_folder/public_folder_cubit.dart
@@ -6,24 +6,32 @@ import 'package:kurs/data/api/public_folder.dart';
 import 'package:kurs/data/models/file.dart';
 import 'package:kurs/data/models/folder.dart';
 import 'package:kurs/data/models/public_folder.dart';
+import 'package:kurs/ui/controllers.dart';
 
 class PublicFolderCubit extends Cubit<PublicFolderState> {
   PublicFolderCubit() : super(PublicFolderEmptyState());
 
-  late List<MPublicFolder> mPFolders;
+  late List<MPublicFolder> mPFolders =[];
   MFolder? mFolder;
   String current = "";
 
   Future<void> load() async {
     emit(PublicFolderLoadingState());
-
+    mPFolders=[];
+    mFolder=null;
     var response = await PublicFolder.getPublicFolders();
     Map<String, dynamic> body = jsonDecode(response.body);
-    List<Map<String, dynamic>> folders = body["data"];
+    List<Map<String, dynamic>> folders = ((body["data"] as List)
+        .map((item) => item as Map<String, dynamic>)
+        .toList());
     for (var data in folders) {
       var mpf = MPublicFolder(name: data["name"], id: data["guid"], data: data);
       mpf.mFolder.parent = mpf;
       mPFolders.add(mpf);
+    }
+    if(current.isNotEmpty){
+      var mpf = mPFolders.firstWhere((element) => element.name==current.split("\\").first);
+      mFolder  = mpf.mFolder.goToPath(current);
     }
 
     Future.delayed(const Duration(milliseconds: 10),
@@ -44,20 +52,37 @@ class PublicFolderCubit extends Cubit<PublicFolderState> {
       mFolder = null;
       current = "";
     }
-    Future.delayed(const Duration(milliseconds: 10),
+    Future.delayed(const Duration(milliseconds: 50),
         () => emit(PublicFolderLoadedState()));
   }
 
   Future<void> renameIO(IOElement io) async{
+    emit(PublicFolderLoadingState());
 
+    if(io is MFolder){
+      await io.rename(Controllers.fileRenameController.text);
+    }
+
+    Future.delayed(const Duration(milliseconds: 50),
+            () => emit(PublicFolderLoadedState()));
   }
 
   Future<void> deleteFile(IOElement io) async{
+    emit(PublicFolderLoadingState());
 
+
+
+    Future.delayed(const Duration(milliseconds: 50),
+            () => emit(PublicFolderLoadedState()));
   }
 
   Future<void> downloadFile(MFile file) async{
+    emit(PublicFolderLoadingState());
 
+
+
+    Future.delayed(const Duration(milliseconds: 50),
+            () => emit(PublicFolderLoadedState()));
   }
 
   Future<void> refresh() async {
